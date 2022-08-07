a94d0ab56c1c23d7945dd336c9105962e0d6a912
Max Bokhan <bokhanworks@gmail.com>
2022-08-07 20:55:42 +0300

    mobile version, except slider

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,13 +1,23 @@
 // Slick Slider Video Carousel
 
-$(document).ready(function(){
+$(function(){
   $('.video__carousel').slick({
+	  lazyLoad: 'onDemand',
 	  slidesToShow: 3,
-	  slidesToScroll: 2,
-	  dots:true,
-	  customPaging : function(slider, i) {
+	  slidesToScroll: 1,
+	  dots: true,
+	  customPaging: function(slider, i) {
 	  	return "<div class='square'></div>";
 	  },
+	  responsive: [
+      {
+        breakpoint: 360,
+        settings: {
+	        slidesToShow: 1,
+			slidesToScroll: 1,
+        }
+      }
+    ]
   });
 });
 
